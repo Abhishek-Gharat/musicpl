ad607568b17cad3005ed82dc3105ea1bf941674d
Abhishek-Gharat <92473353+Abhishek-Gharat@users.noreply.github.com>
2022-01-10 01:43:26 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,87 @@
+
+let allMusic = [
+  {
+    name: "AAj Se Teri",
+    artist: "Arjit Singh",
+    img: "Aaj Se Teri - ( slowed + reverbed ) _ lofi _ lofiwithtwist",
+    src: "Aaj Se Teri - ( slowed + reverbed ) _ lofi _ lofiwithtwist"
+  },
+  {
+    name: "Chammak Challo",
+    artist: "2",
+    img: "Chammak Challo Slowed + Reverbed",
+    src: "Chammak Challo Slowed + Reverbed"
+  },
+  {
+    name: "Criminal",
+    artist: "3",
+    img: "Criminal (RaOne) Slowed + Reverb",
+    src: "Criminal (RaOne) Slowed + Reverb"
+  },
+  {
+    name: "Dhoom taana",
+    artist: "4",
+    img: "dhoom taana (slowed + reverb) shreya ghoshal & abhijeet bhattacharya _ om shanti om",
+    src: "dhoom taana (slowed + reverb) shreya ghoshal & abhijeet bhattacharya _ om shanti om"
+  },
+  {
+    name: "Hans Mat Pagli",
+    artist: "5",
+    img: "Hans Mat Pagli - Sonu Nigam Song _ Slowed And Reverb Lofi Mix",
+    src: "Hans Mat Pagli - Sonu Nigam Song _ Slowed And Reverb Lofi Mix"
+  },
+  {
+    name: "Humdard",
+    artist: "6",
+    img: "Humdard — (slowed and reverb)",
+    src: "Humdard — (slowed and reverb)"
+  },
+  {
+    name: "Inkem Inkem Kavale",
+    artist: "7",
+    img: "Inkem Inkem Kavale (𝑺𝒍𝒐𝒘𝒆𝒅 + 𝑹𝒆𝒗𝒆𝒓𝒃)",
+    src: "Inkem Inkem Kavale (𝑺𝒍𝒐𝒘𝒆𝒅 + 𝑹𝒆𝒗𝒆𝒓𝒃)"
+  },
+  {
+    name: "Kalank ",
+    artist: "8",
+    img: "Kalank [Slowed + Reverb] - Arijit Singh _ SickDude",
+    src: "Kalank [Slowed + Reverb] - Arijit Singh _ SickDude"
+  },
+  {
+    name: "O Re Piya",
+    artist: "9",
+    img: "O Re Piya (Lo-fi 2307 flip) Rahat Fateh Ali Khan _ Bollywood Lofi _ Earphones recommended",
+    src: "O Re Piya (Lo-fi 2307 flip) Rahat Fateh Ali Khan _ Bollywood Lofi _ Earphones recommended"
+  },
+  {
+    name: "Saibo",
+    artist: "10",
+    img: "Saibo x Iktara x Phir se udd chala x Jiyein kyun _ VIBIE's Bollywood Lofi Mix ",
+    src: "Saibo x Iktara x Phir se udd chala x Jiyein kyun _ VIBIE's Bollywood Lofi Mix "
+  },
+  {
+    name: "Saiyaara",
+    artist: "11",
+    img: "Saiyaara [Slowed + Reverb] - Mohit Chauhan _ Lofi Songs _ Indian Lofi Song Channel",
+    src: "Saiyaara [Slowed + Reverb] - Mohit Chauhan _ Lofi Songs _ Indian Lofi Song Channel"
+  },
+  {
+    name: "Tera Fitoor",
+    artist: "12",
+    img: "Tera Fitoor Song ( Slowed + Reverb + Lofi) _ Bollywood Lofi Songs 2021 _ Nexx Beatz",
+    src: "Tera Fitoor Song ( Slowed + Reverb + Lofi) _ Bollywood Lofi Songs 2021 _ Nexx Beatz"
+  },
+  {
+    name: "Titli",
+    artist: "13",
+    img: "titli",
+    src: "titli"
+  },
+  {
+    name: "Yenti Yenti",
+    artist: "14",
+    img: "yenti yenti (slowed + reverbed)",
+    src: "yenti yenti (slowed + reverbed)"
+  },
+];
